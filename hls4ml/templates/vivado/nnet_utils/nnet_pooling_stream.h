b5376964e173ecaf94c074a2181b9b92fd5d54a8
YanLun-Huang <94730012+YanLunHuang@users.noreply.github.com>
2022-09-20 23:12:54 +0800

    Update nnet_pooling_stream.h

--- hls4ml/templates/vivado/nnet_utils/nnet_pooling_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_pooling_stream.h
@@ -853,11 +853,11 @@ template<class data_T, class res_T, typename CONFIG_T>
   
 template<class data_T, class res_T, typename CONFIG_T>
   void pooling_switch(
-            hls::stream<data_T> data[CONFIG_T::data_transfer],
-            hls::stream<res_T>  res[CONFIG_T::data_transfer]
+            hls::stream<data_T> data[CONFIG_T::data_transfer_out],
+            hls::stream<res_T>  res[CONFIG_T::data_transfer_out]
 ) { 
     #pragma HLS inline region
-    if(CONFIG_T::data_transfer == 1){
+    if(CONFIG_T::data_transfer_out == 1){
         pooling_single<data_T, res_T, CONFIG_T>(data, res);
     }else {
         pooling_array<data_T, res_T, CONFIG_T>(data, res);
