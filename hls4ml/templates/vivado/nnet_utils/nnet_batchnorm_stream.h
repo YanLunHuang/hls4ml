2f4e09e1aa879f82d60b93d0e3c3fc4806f65156
YanLun-Huang <94730012+YanLunHuang@users.noreply.github.com>
2022-09-20 23:10:57 +0800

    Update nnet_batchnorm_stream.h

--- hls4ml/templates/vivado/nnet_utils/nnet_batchnorm_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_batchnorm_stream.h
@@ -181,13 +181,13 @@ void normalize_array(
 
 template<class data_T, class res_T, typename CONFIG_T>
 void normalize_switch(
-    hls::stream<data_T> data[CONFIG_T::data_transfer],
-    hls::stream<res_T>  res[CONFIG_T::data_transfer],
+    hls::stream<data_T> data[CONFIG_T::data_transfer_out],
+    hls::stream<res_T>  res[CONFIG_T::data_transfer_out],
     typename CONFIG_T::scale_t scale[CONFIG_T::n_in],
     typename CONFIG_T::bias_t  bias[CONFIG_T::n_in]
 ) {
     #pragma HLS inline region
-    if(CONFIG_T::data_transfer == 1){
+    if(CONFIG_T::data_transfer_out == 1){
         normalize_single<data_T, res_T, CONFIG_T>(data, res, scale, bias);
     }else {
         normalize_array<data_T, res_T, CONFIG_T>(data, res, scale, bias);
